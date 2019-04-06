3768b45998ee2c80e19da0b67fd827313ea1b636
Ravi Kumar <kumar.ravi.ec@gmail.com>
2019-04-06 11:33:39 +0200

    Add case to handle discrepanicies in status

--- lib/services/campaign_discrepancy_finder.rb
+++ lib/services/campaign_discrepancy_finder.rb
@@ -7,13 +7,31 @@ class CampaignDiscrepancyFinder
     @campaigns.each_with_object([]) do |campaign, response|
       local_camp = Campaign.find_by(external_reference: campaign[:reference])
       if discrepancy?(local_camp, campaign)
+        response << build_camp_response(local_camp, campaign)
       end
     end
   end
 
   private
 
+  def build_camp_response(local, remote)
+    {
+      remote_reference: remote[:reference],
+      discrepancies: discrepancies(local, remote)
+    }
+  end
+
   def discrepancy?(local, remote)
-    false
+    local.status != remote[:status] ||
+      local.ad_description != remote[:description]
+  end
+
+  def discrepancies(local, remote)
+    output = []
+    if local.status != remote[:status]
+      output << { status: { remote: remote[:status],
+                            local: local.status } }
+    end
+    output
   end
 end
--- spec/services/campaign_discrepancy_finder_spec.rb
+++ spec/services/campaign_discrepancy_finder_spec.rb
@@ -15,4 +15,30 @@ RSpec.describe CampaignDiscrepancyFinder do
       expect(CampaignDiscrepancyFinder.new(remote_campaign).run).to eq([])
     end
   end
+
+  context 'there are discrepancies between remote and local campaign' do
+    context 'only status is different' do
+      let(:remote_campaign) do
+        [{ reference: '1', status: 'enabled', description: 'Test' }]
+      end
+      let(:expect_output) do
+        [
+          {
+            discrepancies: [
+              {
+                status: {
+                  local: 'active',
+                  remote: 'enabled'
+                }
+              }
+            ],
+            remote_reference: '1'
+          }
+        ]
+      end
+      it 'should include only status in the output' do
+        expect(CampaignDiscrepancyFinder.new(remote_campaign).run).to eq(expect_output)
+      end
+    end
+  end
 end
